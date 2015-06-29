c8de13464f2ab7c9d93f14e26b409addab5e6801
iver56 <iver56@hotmail.com>
2015-06-29 18:49:38 +0200

    Add JSONP support in status.php

--- status.php
+++ status.php
@@ -1,5 +1,6 @@
 <?php
-require_once ('common_include.php');
+
+require_once('common_include.php');
 
 $result = mysqli_query($link, 'SELECT timestamp FROM images ORDER BY id DESC LIMIT 1');
 $data = array();
@@ -15,8 +16,21 @@ if ($result) {
         'is_outdated' => $is_outdated
     );
 
-    header('Content-Type: application/json');
-    echo json_encode($arr);
+    $json_string = json_encode($arr);
+
+    if (array_key_exists('callback', $_GET)) {
+        header('Content-Type: text/javascript; charset=utf8');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Max-Age: 3628800');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+        $callback = $_GET['callback'];
+        echo $callback . '(' . $json_string . ');';
+    } else {
+        header('Content-Type: application/json');
+        echo $json_string;
+    }
 } else {
     die('Error: Invalid query: ' . mysqli_error());
 }
+
