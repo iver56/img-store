a60453507ca6ba2765234b811c94c095d099d3af
iver56 <iver56@hotmail.com>
2015-06-29 18:22:29 +0200

    Fix time format in status endpoint

--- status.php
+++ status.php
@@ -5,14 +5,16 @@ $result = mysqli_query($link, 'SELECT timestamp FROM images ORDER BY id DESC LIM
 $data = array();
 if ($result) {
     $row = mysqli_fetch_assoc($result);
-    
+
+    date_default_timezone_set('UTC');
+    $time = date("c", strtotime($row['timestamp']));
     $is_outdated = strtotime($row['timestamp']) < strtotime('-15 minutes');
-    
+
     $arr = array(
-        'timestamp' => $row['timestamp'],
+        'time' => $time,
         'is_outdated' => $is_outdated
     );
-        
+
     header('Content-Type: application/json');
     echo json_encode($arr);
 } else {
