d9019e406b9f22b2ff628c47bf588242cc1366ae
iver56 <iver56@hotmail.com>
2015-08-10 21:45:26 +0200

    Check hub password

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # img-store
-Store small webcam images in a MySQL database
+
+Store small webcam images in a MySQL database. Made to work in an environment with only free Azure
+services.
+
+# Config
+
+`cp common_include.php.example common_include.php`
+and fill in appropriate values in common_include.php
+
+# Usage
+
+## Put new image:
+POST to /put.php with the "base64" key of the post data set to the image file encoded as base64.
+Also remember to use HTTPS and include the correct "hub_password" in the post data, for security
+reasons.
+
+## Get latest image:
+GET /cam.jpg
+
+## Get status:
+GET /status
--- put.php
+++ put.php
@@ -1,18 +1,21 @@
 <?php
-require_once ('common_include.php');
+require_once('common_include.php');
 
-//TODO: check credentials, and store the hub name
-if (isset($_POST["base64"])) {
-    $base64 = mysqli_real_escape_string($link, $_POST["base64"]);
-    $result = mysqli_query($link, "INSERT INTO images (content) VALUES('$base64')");
-    if (!$result) {
-        printf("Error: %s\n", mysqli_sqlstate($link));
-        exit();
+if (isset($_POST["hub_password"]) && strcmp($_POST["hub_password"], HUB_PASSWORD) === 0) {
+    if (isset($_POST["base64"])) {
+        $base64 = mysqli_real_escape_string($link, $_POST["base64"]);
+        $result = mysqli_query($link, "INSERT INTO images (content) VALUES('$base64')");
+        if (!$result) {
+            printf("Error: %s\n", mysqli_sqlstate($link));
+            exit();
+        } else {
+            mysqli_query($link, "set @old_id = (SELECT id FROM `images` ORDER BY id DESC LIMIT 1 OFFSET 10)");
+            mysqli_query($link, 'DELETE FROM images WHERE id <= @old_id');
+            die('ok');
+        }
     } else {
-        mysqli_query($link, "set @old_id = (SELECT id FROM `images` ORDER BY id DESC LIMIT 1 OFFSET 10)");
-        mysqli_query($link, 'DELETE FROM images WHERE id <= @old_id');
-        die('ok');
+        die('missing post data');
     }
 } else {
-    die('missing post data');
+    die('correct hub pw plz');
 }
